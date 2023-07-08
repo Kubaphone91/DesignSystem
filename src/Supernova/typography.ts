0edb6ac381a557f74a7fcaea0452e167346a8809
Jacob Belica <kuba.coding@gmail.com>
2023-07-08 02:31:13 -0500

    Setup

---
+++ src/Supernova/typography.ts
@@ -0,0 +1,192 @@
+import { Measures } from './measures';
+const headingL = {
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 700,
+  fontSize: {
+    measure: 48,
+    unit: 'Pixels',
+    css: '48px',
+  },
+  lineHeight: {
+    measure: 135,
+    unit: 'Percent',
+    css: '135%',
+  },
+  letterSpacing: {
+    measure: 0,
+    unit: 'Percent',
+    css: '0%',
+  },
+  textIndent: {
+    measure: 0,
+    unit: 'Pixels',
+    css: '0px',
+  },
+};
+const headingM = {
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: {
+    measure: 40,
+    unit: 'Pixels',
+    css: '40px',
+  },
+  lineHeight: {
+    measure: 135,
+    unit: 'Percent',
+    css: '135%',
+  },
+  letterSpacing: {
+    measure: 0,
+    unit: 'Percent',
+    css: '0%',
+  },
+  textIndent: {
+    measure: 0,
+    unit: 'Pixels',
+    css: '0px',
+  },
+};
+const headingS = {
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: {
+    measure: 32,
+    unit: 'Pixels',
+    css: '32px',
+  },
+  letterSpacing: {
+    measure: 0,
+    unit: 'Percent',
+    css: '0%',
+  },
+  textIndent: {
+    measure: 0,
+    unit: 'Pixels',
+    css: '0px',
+  },
+};
+const headingXs = {
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: {
+    measure: 26,
+    unit: 'Pixels',
+    css: '26px',
+  },
+  letterSpacing: {
+    measure: 0,
+    unit: 'Percent',
+    css: '0%',
+  },
+  textIndent: {
+    measure: 0,
+    unit: 'Pixels',
+    css: '0px',
+  },
+};
+const bodyL = {
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: {
+    measure: 20,
+    unit: 'Pixels',
+    css: '20px',
+  },
+  letterSpacing: {
+    measure: 0,
+    unit: 'Percent',
+    css: '0%',
+  },
+  textIndent: {
+    measure: 0,
+    unit: 'Pixels',
+    css: '0px',
+  },
+};
+const bodyM = {
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 700,
+  fontSize: {
+    measure: 18,
+    unit: 'Pixels',
+    css: '18px',
+  },
+  lineHeight: {
+    measure: 100,
+    unit: 'Percent',
+    css: '100%',
+  },
+  letterSpacing: {
+    measure: 0,
+    unit: 'Percent',
+    css: '0%',
+  },
+  textIndent: {
+    measure: 0,
+    unit: 'Pixels',
+    css: '0px',
+  },
+};
+const bodyS = {
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: {
+    measure: 16,
+    unit: 'Pixels',
+    css: '16px',
+  },
+  lineHeight: {
+    measure: 135,
+    unit: 'Percent',
+    css: '135%',
+  },
+  letterSpacing: {
+    measure: 0,
+    unit: 'Percent',
+    css: '0%',
+  },
+  textIndent: {
+    measure: 0,
+    unit: 'Pixels',
+    css: '0px',
+  },
+};
+const microcopy = {
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: {
+    measure: 12,
+    unit: 'Pixels',
+    css: '12px',
+  },
+  letterSpacing: {
+    measure: 0,
+    unit: 'Percent',
+    css: '0%',
+  },
+  textIndent: {
+    measure: 0,
+    unit: 'Pixels',
+    css: '0px',
+  },
+};
+
+export const Typographies = {
+  headingL,
+  headingM,
+  headingS,
+  headingXs,
+  bodyL,
+  bodyM,
+  bodyS,
+  microcopy,
+};
