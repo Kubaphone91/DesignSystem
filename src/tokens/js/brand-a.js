8e906c5ea28dca725f8c096711b7262eb6ca8dcb
Kubaphone91 <belica.j.92@gmail.com>
2023-12-07 04:50:42 +0000

    copy css tokens

---
+++ src/tokens/js/brand-a.js
@@ -0,0 +1,25 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 07 Dec 2023 04:50:21 GMT
+ */
+
+export const ColorPrimaryDefault = "#0f992e";
+export const ColorPrimaryHover = "#0d8227";
+export const ColorPrimaryDisabled = "#c2d0dc";
+export const ButtonRadius = "48px";
+export const ButtonPaddingLeft = "16px";
+export const ButtonPaddingRight = "16px";
+export const ButtonHeightLg = "36px";
+export const ButtonHeightSm = "24px";
+export const ButtonTypographyLgFontFamily = "'Consolas', Courier, monospace";
+export const ButtonTypographyLgFontWeight = 500;
+export const ButtonTypographyLgFontSize = "16px";
+export const ButtonTypographyLgTextCase = "none";
+export const ButtonTypographySmFontFamily = "'Consolas', Courier, monospace";
+export const ButtonTypographySmFontWeight = 500;
+export const ButtonTypographySmFontSize = "12px";
+export const ButtonTypographySmTextCase = "none";
+export const ButtonFontFamily = "'Consolas', Courier, monospace";
+export const ColorDisabledText = "#a3a3a3";
+export const ColorDisabledBackground = "#f5f5f5";
+export const ColorDisabledBorder = "#adadad";
---
+++ src/tokens/js/brand-b.js
@@ -0,0 +1,25 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 07 Dec 2023 04:50:21 GMT
+ */
+
+export const ColorPrimaryDefault = "#840977";
+export const ColorPrimaryHover = "#700865";
+export const ColorPrimaryDisabled = "#e0c2dd";
+export const ButtonRadius = "4px";
+export const ButtonPaddingLeft = "16px";
+export const ButtonPaddingRight = "16px";
+export const ButtonHeightLg = "36px";
+export const ButtonHeightSm = "24px";
+export const ButtonTypographyLgFontFamily = "'Inter', Helvetica, Arial, sans-serif";
+export const ButtonTypographyLgFontWeight = 400;
+export const ButtonTypographyLgFontSize = "16px";
+export const ButtonTypographyLgTextCase = "none";
+export const ButtonTypographySmFontFamily = "'Inter', Helvetica, Arial, sans-serif";
+export const ButtonTypographySmFontWeight = 400;
+export const ButtonTypographySmFontSize = "12px";
+export const ButtonTypographySmTextCase = "none";
+export const ButtonFontFamily = "'Inter', Helvetica, Arial, sans-serif";
+export const ColorDisabledText = "#a3a3a3";
+export const ColorDisabledBackground = "#f5f5f5";
+export const ColorDisabledBorder = "#adadad";
